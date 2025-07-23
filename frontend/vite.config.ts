36002d2c1fe64438a9ec5a869fc3d03472fc567e
felixkrutov <googoldecaplex@bk.ru>
2025-07-24 00:15:25 +0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  base: '/hub/',
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 3000,
     proxy: {
       '/mossaassistant/api': {
         target: 'http://backend:8000',
