a8d3ee188775e7e4ebc95b9627c157741f667326
felixkrutov <googoldecaplex@bk.ru>
2025-07-23 22:06:50 +0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/mossaassistant/api': {
+        target: 'http://backend:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/mossaassistant\/api/, '/mossaassistant/api'),
+      },
+    },
+  },
 });
