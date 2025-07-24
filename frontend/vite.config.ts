7c046feb9dd164bb372c05fde2ab4b2d2c5833ee
felixkrutov <googoldecaplex@bk.ru>
2025-07-24 00:22:17 +0000

    FIX: Apply correct base path '/hub/' to solve white screen

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hub/',
   plugins: [react()],
+  // Указываем Vite, что все пути должны начинаться с /hub/
+  base: '/hub/',
   server: {
     host: '0.0.0.0',
     port: 3000,
-    proxy: {
-      '/mossaassistant/api': {
-        target: 'http://backend:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/mossaassistant\/api/, '/mossaassistant/api'),
-      },
-    },
-  },
-});
+  }
+})
